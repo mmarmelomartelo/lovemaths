f2ea3be2e46cd167eb6f1e2fd510067de382dea9
Marcelo  Montenegro <montenegromf@gmail.com>
2023-03-10 09:44:52 +0000

    basic js function are added

--- assets/js/scrpt.js
+++ assets/js/scrpt.js
@@ -1,2 +1,37 @@
 console.log("We are connected");
-console.log("We are connected");
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
