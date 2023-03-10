ba21be1092605c65ab5adaefa19861b46eecdd0a
Marcelo  Montenegro <montenegromf@gmail.com>
2023-03-10 10:45:00 +0000

    Add code to calculate two random numbers for the game question

--- assets/js/scrpt.js
+++ assets/js/scrpt.js
@@ -8,15 +8,19 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
-                let gameType = his.getAttribute("data-type");
+                let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
             }
         })
     }
 })
 
+/**The main game "loop", called when the script is first loaded
+ * and after the user´s answer has been processed
+ */
 function runGame() {
-
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
 }
 
 function checkAnswer() {
