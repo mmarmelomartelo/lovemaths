10ca6959c0e0d05f86c01ee7eb80217e74a24fad
Marcelo  Montenegro <montenegromf@gmail.com>
2023-03-10 10:22:39 +0000

    js code to wait DOM be loaded and button elements and listeners added

--- assets/js/scrpt.js
+++ assets/js/scrpt.js
@@ -1,5 +1,20 @@
 console.log("We are connected");
 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = his.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
